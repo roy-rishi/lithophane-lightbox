4bf0216647fa70e23affef06bb731fa89eecdd84
roy-rishi <72419270+roy-rishi@users.noreply.github.com>
2026-08-24 23:03:08 -0700

    partial BLE implementation; ignore .vscode directory

--- firmware/main/config.h
+++ firmware/main/config.h
@@ -4,5 +4,9 @@
 
 // Pin definitions
 #define STATUS_LED_PIN 2
+// Logging
+#define TAG "app"
+// Bluetooth
+#define GAP_NAME "Lightbox"
 
 #endif  // CONFIG_H_
--- firmware/main/main.c
+++ firmware/main/main.c
@@ -1,13 +1,41 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include <nvs_flash.h>
 #include <stdio.h>
 
 #include "config.h"
+#include "esp_log.h"
+#include "host/ble_hs.h"
+#include "host/ble_uuid.h"
+#include "host/util/util.h"
 #include "led.h"
+#include "nimble/ble.h"
+#include "nimble/nimble_port.h"
+#include "nimble/nimble_port_freertos.h"
 
 void app_main(void) {
-    printf("Initializing status LED on pin %d...\n", STATUS_LED_PIN);
+    // initialize status LED
+    ESP_LOGI(TAG, "Initializing LED on pin %d", STATUS_LED_PIN);
     led_init();
+
+    // initialize NVS flash storage, erasing it on error and trying again
+    ESP_LOGI(TAG, "Initializing NVS flash");
+    esp_err_t err = nvs_flash_init();
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_LOGW(TAG, "Erasing flash");
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ESP_ERROR_CHECK(nvs_flash_init());
+    }
+
+    // initialize NimBLE stack
+    ESP_LOGI(TAG, "Initializing NimBLE");
+    ESP_ERROR_CHECK(nimble_port_init());
+
+    // initialize GAP service for advertising
+    ble_svc_gap_init();
+    ble_svc_gap_device_name_set(GAP_NAME);
+
+    // TODO...
 
     while (1) {
         led_on();
