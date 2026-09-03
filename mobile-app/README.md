f47765d879eaaff359869ee7f628c4fb5f58953f
roy-rishi <72419270+roy-rishi@users.noreply.github.com>
2026-09-02 17:46:05 -0700

    remove mobile app project

--- mobile-app/README.md
+++
@@ -1,3 +0,0 @@
-# lithopane
-
-A new Flutter project.
--- mobile-app/android/app/src/main/kotlin/com/rishiroy/lithopane/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.rishiroy.lithopane
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
